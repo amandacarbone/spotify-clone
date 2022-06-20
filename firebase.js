a3b8c0596d1e88f140266c39462b1bb87d6b4d18
Amanda Carbone <amanda.elyce.carbone@gmail.com>
2022-06-19 22:58:40 -0400

    deleted firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBv0av5KMv6SqDKEpSPpEHx6o9nkf4yRd8",
-    authDomain: "spotify-clone-2b0f5.firebaseapp.com",
-    projectId: "spotify-clone-2b0f5",
-    storageBucket: "spotify-clone-2b0f5.appspot.com",
-    messagingSenderId: "841182018409",
-    appId: "1:841182018409:web:df4662b8b55bf009f95bf8"
-};
